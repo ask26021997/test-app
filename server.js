5d31a4a1b3e871fee7c234e369dd8681f1a0f377
Anand Singh <ask26021997@gmail.com>
2022-08-28 18:50:10 +0530

    testing error fix

--- server.js
+++ server.js
@@ -18,21 +18,19 @@ app.use(express.static("public"));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 
-// const db = mongoose.connection;
-// db.on("error", (err) => {
-//   console.log(err);
-// });
-// db.once("open", () => {
-//   console.log("Connected to mongoDB");
+const db = mongoose.connection;
+db.on("error", (err) => {
+  console.log(err);
+});
+db.once("open", () => {
+  console.log("Connected to mongoDB");
+});
 
-//   // console.log(db.collections);
-// });
-
-// mongoose.connect(process.env.DATABASE_URL, {
-//   useNewUrlParser: true,
-// });
+mongoose.connect(process.env.DATABASE_URL, {
+  useNewUrlParser: true,
+});
 
 // app.use("/", indexRouter);
 // app.use("/authors", authorRouter);
